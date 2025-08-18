06d28be569d4639e4ff2c0dd4dbe69cc5abdf471
Awosch28 <awosch.gaming@gmail.com>
2025-08-17 23:37:26 -0500

    adjust pipeline for initializing the db

--- webapp/app.py
+++ webapp/app.py
@@ -77,7 +77,7 @@ app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Naive database setup
+# Naive databse setup
 try:
     init_db_command()
 except sqlite3.OperationalError:
