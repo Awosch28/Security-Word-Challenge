efa415fb4ad0cb2f318453a9dafddf56a7f7820c
Awosch28 <awosch.gaming@gmail.com>
2025-09-01 22:37:23 -0500

    fixed typo in game results

--- webapp/static/game.js
+++ webapp/static/game.js
@@ -110,7 +110,7 @@ const app = Vue.createApp({
         if (localStorage.getItem("game_results") === null) {
             this.game_results = {};
             this.game_results[this.config.language_code] = [];
-            localStorage.setItem("game_result", JSON.stringify(this.game_results));
+            localStorage.setItem("game_results", JSON.stringify(this.game_results));
         } else {
             this.game_results = JSON.parse(localStorage.getItem("game_results"));
             if (!this.game_results[this.config.language_code]) {
